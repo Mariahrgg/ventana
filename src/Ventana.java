e33dc9e0adb1ccbc08b76287e09e691a75677d55
Mariahrgg <mhernandezramos@danielcastelao.org>
2022-10-14 09:51:24 +0200

    subo todo

---
+++ src/Ventana.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+/**
+ * @version 1.0
+ */
+public class Ventana {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Hola!");
+    }
+}
+
---
+++ src/multiejecucion.java
@@ -0,0 +1,9 @@
+public class multiejecucion {
+
+    public static void main(String[] args) {
+        System.out.println("holi ");
+    }
+
+
+
+}
